e37d94abd82f3a5fabf373b15dc07ad24e846022
Tomasz Staniewicz <tomasz.staniewicz@deviniti.com>
2019-05-03 11:40:36 +0200

    Initial commit

---
+++ src/main/java/io/staniewicz/SingleThreadedBlockingServer.java
@@ -0,0 +1,34 @@
+package io.staniewicz;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SingleThreadedBlockingServer {
+
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8080);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            handle(clientSocket);
+        }
+    }
+
+    private static void handle(Socket clientSocket) throws IOException {
+        try (
+            clientSocket;
+            OutputStream out = clientSocket.getOutputStream();
+            InputStream in = clientSocket.getInputStream();
+        ) {
+            int data;
+            while ((data = in.read()) != -1) {
+                out.write(Character.toUpperCase(data));
+            }
+        }
+    }
+}
